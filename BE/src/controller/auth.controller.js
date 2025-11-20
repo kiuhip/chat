1c79bb5fe954c08cceef76807c22b9150a1b0233
kiuhip <hiep.kt235325@sis.hust.edu.vn>
2025-11-20 23:39:06 +0700

    login and logout endpoint

--- BE/src/controller/auth.controller.js
+++ BE/src/controller/auth.controller.js
@@ -3,6 +3,7 @@ import { generateToken } from "../lib/untils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { ENV } from "../lib/env.js";
+import { response } from "express";
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
@@ -61,4 +62,37 @@ export const signup = async (req, res) => {
         res.status(500).json({ message: "Internal server error" });
     }
 
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: "Vui lòng điền đầy đủ thông tin" });
+    }
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ message: "Không tìm thấy người dùng" });
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) return res.status(400).json({ message: "Mật khẩu không chính xác" });
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+    } catch (error) {
+        console.log("Error logging in:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const logout = async (_, res) => {
+    res.cookies("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Đăng xuất thành công" });
 };
